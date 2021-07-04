eca916ecd83ab11b9cb5545141ed5f8c233ec3ca
leitong <leitongchen93@gmail.com>
2021-07-04 14:02:48 +0200

    added foreign keys; created relations

--- app/Post.php
+++ app/Post.php
@@ -11,4 +11,14 @@ class Post extends Model
         "content",
         "slug",
     ];
+
+    public function category() 
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function user() 
+    {
+        return $this->belongsTo('App\User');
+    }
 }
