a23769c2bf358547b608cad0f6b5bf3c48e147a6
leitong <leitongchen93@gmail.com>
2021-07-09 16:51:24 +0200

    changed guest show blade layout

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,24 +2,102 @@
 
 @section('link-section')
 
-    <a href="{{ route('posts.index') }}">Back to overview</a>
+    <div class="d-flex justify-content-start">
+    
+        <a href="{{ route('posts.index') }}">Back to overview</a>
+
+    </div>
 
 @endsection
 
-
-
 @section('content')
 
+    <div class="post-container">
     
+        <h1>{{ $post->title }}</h1>
 
-    {{-- <h1>Pagina show che mostra solo un unico post</h1> --}}
+        <div class="head-box d-flex align-items-center">
 
-    <p>{{ $post->id }}</p>
-    <h2>{{ $post->title }}</h2>
+            <div class="mr-5 tag">
+                ID:  
+                <span>
+                    <strong>{{ $post->id }}</strong>
+                </span>
+            </div>
 
-    <p>{{ $post->content }}</p>
+            <div class="slug-box flex-grow-1 d-flex align-items-center tag">
+                Slug: 
+                <span class="flex-grow-1">
+                    {{ $post->slug }}
+                </span>
+            </div>
 
-    <p>{{ $post->slug }}</p>
+        </div>  
+        
+        @if($post->src)
+            
+            <div class="img-box">
+                <img src="{{$post->src}}" alt="random image">
+            </div>
 
+        @endif
+
+        <div class="content-box">
+        
+            <p>
+                {{ $post->content }}
+            </p>
+
+        </div>
+
+        <div class="info-box">
+
+            <div>
+                <h6>Author:</h6>
+                <div class="info-text">
+                    <a href="#">
+                    
+                        {{ $post->user->name }}
+
+                    </a>
+                </div>
+            </div>
+
+            <div>
+                <h6>Category:</h6>
+                <div class="info-text">
+                    <a href="{{ route('posts.filter', ['category' => $post->category['id']]) }}">
+                    
+                        {{ $post->category ? $post->category['name'] : "-" }}
+                    
+                    </a>
+                </div>
+            </div>
+
+            <div>
+                <h6>Tags:</h6>
+
+                <div class="info-text">
+                    @if(count($post->tags)>0)
+                    @foreach($post->tags as $tag)
+
+                        <div class="badge badge-info">
+                            <a href="{{ route('posts.filter', ['tag' => $tag->id]) }}" class="text-white">
+                                {{ $post->tags ? $tag['name'] : "-" }}
+                            </a>
+                        </div>
+
+                    @endforeach
+                    @else
+                        <span>No tags</span>
+                    @endif
+
+                </div>
+            </div>
+            
+
+        </div>
+
+    </div>
 
 @endsection
